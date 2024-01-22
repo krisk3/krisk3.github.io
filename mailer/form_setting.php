44e0d06560c30d530d56d613d2bdd7b36fe90499
Kuriakose <kris.kuruvilla@gmail.com>
2024-01-22 19:59:44 +0530

    Portfolio Website.

---
+++ mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "New message from Geeky Dev"; //letter subject
+	$to = 'kris.kuruvilla@gmail.com'; // Enter Your E-mail
+	$from = 'kris.kuruvilla+3@gmail.com'; // Admin e-mail
+	$fromName = 'Geeky Dev'; // Your company name
+	$charset = 'UTF-8';
+?>
